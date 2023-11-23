1dc671c56619d1069e6d2ce15d341414215ef39e
naufyl28 <11211060@student.itk.ac.id>
2023-11-23 14:20:08 +0800

    cart

--- front-end/src/pages/Cart.jsx
+++ front-end/src/pages/Cart.jsx
@@ -24,23 +24,7 @@ function Cart() {
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-8">
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Image</span>
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Product
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Qty
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" className="px-6 py-3">
-                Action
-              </th>
-            </tr>
+           
           </thead>
           <tbody>
             <tr
@@ -85,7 +69,7 @@ function Cart() {
           <div className="checkout-container">
             <button
               type="button"
-              className="text-white bg-green-500 hover-bg-blue-800 focus-ring-4 focus-outline-none focus-ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark-bg-blue-600 dark-hover-bg-blue-700 dark-focus-ring-blue-800"
+              className="text-black custom-background font-semibold bg-yellow-300 hover-bg-blue-800 focus-ring-4 focus-outline-none focus-ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark-bg-blue-600 dark-hover-bg-blue-700 dark-focus-ring-blue-800"
               id="checkout-button"
             >
               <svg
