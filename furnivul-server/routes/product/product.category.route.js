644b94e17b3893063f2920af759d51e7e58cdff2
Naufal Alief <naufalalief087@gmail.com>
2023-11-23 21:38:03 +0700

    fix category and type auth no need to auth

--- furnivul-server/routes/product/product.category.route.js
+++ furnivul-server/routes/product/product.category.route.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const productCategoryController = require("../../controllers/product/product.category.controller");
-const { isAdmin } = require("../../middleware/auth");
+const { isAdmin, auth } = require("../../middleware/auth");
 
 router.get("/", productCategoryController.getAllData);
 router.get("/:id", productCategoryController.getDatabyID);
-router.put("/:id",isAdmin, productCategoryController.updateData);
-router.delete("/:id",isAdmin, productCategoryController.deleteData);
-router.post("/",isAdmin, productCategoryController.addData);
+router.put("/:id", auth, isAdmin, productCategoryController.updateData);
+router.delete("/:id", auth, isAdmin, productCategoryController.deleteData);
+router.post("/", auth, isAdmin, productCategoryController.addData);
 
 module.exports = router;
