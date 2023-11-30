ab586300b6c367c011fc39daaa54b2ca6ae6e2c6
naufyl28 <11211060@student.itk.ac.id>
2023-12-01 04:17:31 +0800

    payment dan hasil payment

--- front-end/src/pages/Payment.jsx
+++ front-end/src/pages/Payment.jsx
@@ -1,84 +1,147 @@
-import { Breadcrumb, Button, Card } from "flowbite-react";
+import { Breadcrumb, Button, Card, Modal } from "flowbite-react";
 import { FaCartShopping } from "react-icons/fa6";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Payment() {
+  const [showQrisModal, setShowQrisModal] = useState(false);
+  const [showUnderMaintenanceModal, setShowUnderMaintenanceModal] = useState(false); // Memperbaiki nama variabel
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    // Retrieve data from local storage
+    const storedTotalPrice = localStorage.getItem("totalPrice");
+    if (storedTotalPrice) {
+      setTotalPrice(parseInt(storedTotalPrice, 10));
+    }
+  }, []);
+
+  const openQrisModal = () => {
+    setShowQrisModal(true);
+  };
+
+  const closeQrisModal = () => {
+    setShowQrisModal(false);
+  };
+
+  const openUnderMaintenanceModal = () => {
+    setShowUnderMaintenanceModal(true);
+  };
+
+  const closeUnderMaintenanceModal = () => {
+    setShowUnderMaintenanceModal(false);
+  };
+
   return (
     <div className="mx-7">
       <Breadcrumb
         aria-label="Solid background breadcrumb example"
-        className="bg-gray-50  py-3 dark:bg-gray-800"
+        className="bg-gray-50 py-3 dark:bg-gray-800"
       >
-        <Breadcrumb.Item href="#" icon={FaCartShopping}>
-          Home
-        </Breadcrumb.Item>
-        <Breadcrumb.Item href="#">Cart</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">Address</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">Checkout</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">Payment</Breadcrumb.Item>
+        {/* Breadcrumb items */}
       </Breadcrumb>
-      <h1 className="text-3xl font-bold">Payment</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+
+      <h1 className="flex items-center justify-center text-3xl font-bold">
+        Payment
+      </h1>
+      <div className="flex items-center justify-center">
         <div>
           <Card className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden mt-4 h-full">
-            <Card className="flex border-2 border-yellow-400" active>
-              <div className="flex items-center justify-center ">
+            <Button onClick={openQrisModal}>
+              <div className="flex items-center justify-center">
                 <img
                   src="https://xendit.co/wp-content/uploads/2020/03/iconQris.png"
-                  alt="qris"
+                  alt="Qrisbutton"
                   width={150}
                 />
               </div>
+            </Button>
+            <Card className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden mt-4 h-full">
+              <Button onClick={openUnderMaintenanceModal}>
+                <div className="flex items-center justify-center">
+                  <img
+                    src="https://image.cermati.com/v1585904886/o81yliwckjhywelnx13a.png"
+                    alt="gambar2"
+                    width={150}
+                  />
+                </div>
+              </Button>
             </Card>
             <Card className="flex">
-              <div className="flex items-center justify-center">
-                <img
-                  src="https://image.cermati.com/v1585904886/o81yliwckjhywelnx13a.png"
-                  alt="qris"
-                  width={150}
-                />
-              </div>
+              <Button onClick={openUnderMaintenanceModal}>
+                <div className="flex items-center justify-center">
+                  <img
+                    src="https://cdn.worldvectorlogo.com/logos/paypal-3.svg"
+                    alt="gambar3"
+                    width={150}
+                  />
+                </div>
+              </Button>
             </Card>
             <Card className="flex">
-              <div className="flex items-center justify-center">
-                <img
-                  src="https://cdn.worldvectorlogo.com/logos/paypal-3.svg"
-                  alt="qris"
-                  width={150}
-                />
-              </div>
-            </Card>
-            <Card className="flex">
-              <div className="flex items-center justify-center">
-                <img
-                  src="https://e7.pngegg.com/pngimages/1013/540/png-clipart-mastercard-mastercard.png"
-                  alt="qris"
-                  width={150}
-                />
-              </div>
+              <Button onClick={openUnderMaintenanceModal}>
+                <div className="flex items-center justify-center">
+                  <img
+                    src="https://e7.pngegg.com/pngimages/1013/540/png-clipart-mastercard-mastercard.png"
+                    alt="gambar4"
+                    width={150}
+                  />
+                </div>
+              </Button>
             </Card>
           </Card>
         </div>
-        <div>
-          <Card className="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden mt-4 h-full items-center text-center">
-            <div>
-              <h1 className="text-3xl font-bold">Scan QRIS</h1>
-              <p>Scan QRIS untuk pembayaran</p>
-            </div>
+      </div>
+
+      {/* Modal for Qris */}
+      <Modal
+        show={showQrisModal}
+        onClose={closeQrisModal}
+        className="modal-container"
+      >
+        <Modal.Header className="">Scan QRIS</Modal.Header>
+        <Modal.Body className="modal-content">
+          <div className="flex flex-col items-center justify-center">
+            <p className="font-bold text-2xl mt-4">
+              Total Price:{" "}
+              {totalPrice.toLocaleString("id-ID", {
+                style: "currency",
+                currency: "IDR",
+              })}
+            </p>
+            <p className="mt-3 mb-3">Scan QRIS for payment</p>
+          </div>
+
+          <div className="flex flex-col items-center justify-center">
             <img
               src="https://tabungwakafumat.org/wp-content/uploads/2021/07/QR-Code.jpg"
               alt="qris"
               width={300}
               height={350}
             />
+          </div>
+        </Modal.Body>
+        <Modal.Footer className="flex items-center justify-center">
+          <Button>
+            <Link to="transaction-status">Transaction Status</Link>
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
-            <Button>
-              {" "}
-              <Link to="transaction-status">Transaction Status </Link>
-            </Button>
-          </Card>
-        </div>
-      </div>
+      {/* Modal for Under Maintenance */}
+      <Modal
+        show={showUnderMaintenanceModal}
+        onClose={closeUnderMaintenanceModal}
+        className="modal-container"
+      >
+        <Modal.Header className="">Under Maintenance</Modal.Header>
+        <Modal.Body className="modal-content">
+          <p>Sorry, this payment is currently under maintenance.</p>
+        </Modal.Body>
+        <Modal.Footer className="flex items-center justify-center">
+          <Button onClick={closeUnderMaintenanceModal}>Accept</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
--- front-end/src/pages/TransactionStatus.jsx
+++ front-end/src/pages/TransactionStatus.jsx
@@ -9,7 +9,7 @@ function TransactionStatus() {
     <div className="mx-7">
       <Breadcrumb
         aria-label="Solid background breadcrumb example"
-        className="bg-gray-50  py-3 dark:bg-gray-800"
+        className="bg-gray-50 py-3 dark:bg-gray-800"
       >
         <Breadcrumb.Item href="#" icon={FaCartShopping}>
           Home
@@ -20,34 +20,40 @@ function TransactionStatus() {
         <Breadcrumb.Item href="#">Payment</Breadcrumb.Item>
         <Breadcrumb.Item href="#">Payment Status</Breadcrumb.Item>
       </Breadcrumb>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 item-center justify-center">
-        <Card className="items-center">
-          <div className=" justify-center items-center space-y-4">
-            <div>
-              <h1 className="text-3xl font-bold">Payment Status</h1>
+
+      <div className="flex items-center justify-center mt-28 mb-16">
+        <div className="items-center justify-center">
+          <Card className="items-center">
+            <div className="text-center justify-center items-center space-y-4">
+              <div className="justify-center items-center">
+                <h1 className="text-3xl font-bold">Payment Status</h1>
+              </div>
+              <div className="items-center">
+                <img
+                  src="https://cdn-icons-png.flaticon.com/512/8215/8215539.png"
+                  width={100}
+                  className="justify-center items-center mx-auto mt-10"
+                  alt="Payment Success"
+                />
+              </div>
+              <div className="text-center">
+                <h1 className="text-xl font-semibold mb-3">
+                  Payment was successful
+                </h1>
+                <p className="text-md font-semibold">
+                  Payment has been received successfully. Thank you for your
+                  payment
+                </p>
+              </div>
+              <div className="flex justify-center gap-3">
+                <Button href="/" color="blue" hoverColor="darkgreen">
+                  Back to Home
+                </Button>
+                {/* <Button color="blue">Download Receipt</Button> */}
+              </div>
             </div>
-            <div className="items-center">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/8215/8215539.png"
-                width={100}
-                className="justify-center items-center  mx-auto"
-              />
-            </div>
-            <div className="text-center">
-              <h1 className="text-xl font-semibold">Payment was successful</h1>
-              <p className="text-md font-semibold justify-center ">
-                Payment has been received successfully. Thank you for your
-                payment
-              </p>
-            </div>
-            <div className="flex justify-center gap-3">
-              <Button href="/" color="green">
-                Back to Home
-              </Button>
-              <Button color="blue">Download Reicept</Button>
-            </div>
-          </div>
-        </Card>
+          </Card>
+        </div>
       </div>
     </div>
   );
