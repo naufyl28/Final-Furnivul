5dd25815b860677688ad7b54fbae8e7733dbe43e
naufyl28 <11211060@student.itk.ac.id>
2023-11-30 17:33:36 +0800

    cart dan checkout blm fix

--- front-end/src/pages/Cart.jsx
+++ front-end/src/pages/Cart.jsx
@@ -1,144 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { Breadcrumb, Button, Modal } from "flowbite-react";
+import { Breadcrumb, Button, Card, Label, Modal, Select } from "flowbite-react";
 import { FaCartShopping } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
-import { Button as FlowbiteButton } from "flowbite-react";
 
-function Cart() {
-  const [datas, setData] = useState({ message: "", data: [] });
-  const [deleteIndex, setDeleteIndex] = useState(null);
+function Checkout() {
   const [openModal, setOpenModal] = useState(false);
-  const [voucherModal, setVoucherModal] = useState(false);
-  const [selectedVoucher, setSelectedVoucher] = useState(null);
-  const [voucherData, setVoucherData] = useState(null);
-  const [useVoucher, setUseVoucher] = useState(false);
-
-  const productCart = JSON.parse(localStorage.getItem("cart") || "[]");
-
+  const [courierData, setCourierData] = useState([]);
+  const [selectedCourier, setSelectedCourier] = useState("");
   const token = JSON.parse(localStorage.getItem("token"));
 
-  const fetchVoucherData = () => {
-    return axios
-      .get("https://furnivul-web-app-production.up.railway.app/voucher", {
+  // Additional data from localStorage
+  const phone = JSON.parse(localStorage.getItem("phone"));
+  const province = JSON.parse(localStorage.getItem("province"));
+  const district = JSON.parse(localStorage.getItem("district"));
+  const subdistrict = JSON.parse(localStorage.getItem("subdistrict"));
+  const zipcode = JSON.parse(localStorage.getItem("zipcode"));
+
+  // Additional product data from localStorage
+  const productData = JSON.parse(localStorage.getItem("productData")) || [];
+
+  useEffect(() => {
+    axios(
+      "https://furnivul-web-app-production.up.railway.app/courier-services",
+      {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      })
+      }
+    )
       .then((result) => {
-        setVoucherData(result.data);
+        setCourierData(result.data.data);
       })
       .catch((error) => {
-        console.error("Error mengambil data voucher:", error);
-        if (error.response) {
-          console.error("Detail respons:", error.response.data);
-        }
-        return Promise.reject(error);
+        console.error("Error fetching courier data:", error);
+        console.log("Error response data:", error.response.data);
       });
-  };
+  }, [token]);
 
-  useEffect(() => {
-    fetchVoucherData();
-
-    // Perbarui state datas.data dari localStorage saat komponen dimuat
-    setData({ ...datas, data: productCart });
-  }, []);
-
-  const handleIncrement = (index) => {
-    const updatedData = [...datas.data];
-    const product = updatedData[index];
-
-    const existingProductIndex = productCart.findIndex(
-      (item) => item.product_id === (product && product.product_id)
-    );
-
-    if (existingProductIndex !== -1) {
-      // Produk sudah ada di keranjang, tambahkan kuantitas
-      productCart[existingProductIndex].quantity += 1;
-      setData({
-        ...datas,
-        data: updatedData.map((item, i) =>
-          item.product_id === productCart[existingProductIndex].product_id
-            ? { ...item, quantity: productCart[existingProductIndex].quantity }
-            : item
-        ),
-      });
-    } else {
-      // Produk belum ada di keranjang, tambahkan baru
-      const newProduct = { ...product, quantity: 1 };
-      productCart.push(newProduct);
-      setData({
-        ...datas,
-        data: [...productCart],
-      });
-    }
-
-    localStorage.setItem("cart", JSON.stringify(productCart));
-    fetchVoucherData();
-  };
-
-  // ...
-
-  const handleDecrement = (index) => {
-    const updatedData = [...datas.data];
-    const product = updatedData[index];
-
-    const existingProductIndex = productCart.findIndex(
-      (item) => item.product_id === (product && product.product_id)
-    );
-
-    if (existingProductIndex !== -1) {
-      if (productCart[existingProductIndex].quantity > 1) {
-        // Produk sudah ada di keranjang, kurangi kuantitas
-        productCart[existingProductIndex].quantity -= 1;
-        setData({
-          ...datas,
-          data: updatedData.map((item, i) =>
-            item.product_id === productCart[existingProductIndex].product_id
-              ? {
-                  ...item,
-                  quantity: productCart[existingProductIndex].quantity,
-                }
-              : item
-          ),
-        });
-      } else {
-        // Set deleteIndex ke nilai yang sesuai
-        setDeleteIndex(existingProductIndex);
-        setOpenModal(true);
-        return;
-      }
-    }
-
-    localStorage.setItem("cart", JSON.stringify(productCart));
-    fetchVoucherData();
-  };
-
-  const handleDeleteItem = () => {
-    if (deleteIndex !== null) {
-      const updatedData = datas.data.filter(
-        (_, index) => index !== deleteIndex
-      );
-      setData({ ...datas, data: updatedData });
-      setDeleteIndex(null);
-      setOpenModal(false);
-
-      // Perbarui localStorage setelah menghapus item
-      const updatedCart = [...productCart];
-      updatedCart.splice(deleteIndex, 1);
-      localStorage.setItem("cart", JSON.stringify(updatedCart));
-      fetchVoucherData();
-    }
-  };
-
-  const handleVoucherSelect = (voucher) => {
-    setSelectedVoucher(voucher);
-    setVoucherModal(false);
-  };
-
-  const handleVoucherCheckbox = () => {
-    setUseVoucher(!useVoucher);
-    setSelectedVoucher(null);
+  const selectCourier = (event) => {
+    setSelectedCourier(event.target.value);
   };
 
   const formatCurrency = (value) => {
@@ -149,18 +50,18 @@ function Cart() {
   };
 
   const calculateTotalPrice = () => {
-    const totalPriceWithoutDiscount = Array.isArray(datas.data)
-      ? datas.data.reduce(
-          (total, item) =>
-            total + (item.quantity || 0) * (item.product_price || 0),
-          0
-        )
-      : 0;
-
-    return useVoucher && selectedVoucher
-      ? Math.max(totalPriceWithoutDiscount - (selectedVoucher.discount || 0), 0)
-      : Math.max(totalPriceWithoutDiscount, 0);
+    return productData.reduce(
+      (total, item) => total + (item.quantity || 0) * (item.product_price || 0),
+      0
+    );
   };
+
+  const handleCheckout = () => {
+    // Implement your checkout logic here
+    // For example, you can send an API request to finalize the order
+    console.log("Checkout logic goes here!");
+  };
+
   return (
     <>
       <Breadcrumb
@@ -170,163 +71,62 @@ function Cart() {
         <Breadcrumb.Item key="home" href="/" icon={FaCartShopping}>
           Beranda
         </Breadcrumb.Item>
-        <Breadcrumb.Item key="cart" href="#" className="">
-          Keranjang
+        <Breadcrumb.Item key="checkout" href="#" className="">
+          Checkout
         </Breadcrumb.Item>
       </Breadcrumb>
 
-      <Button className="">
-        <NavLink to={"address"}> Alamat </NavLink>
-      </Button>
+      <div>
+        <Card className="w-full">
+          <h1 className="text-2xl font-semibold">Address</h1>
 
-      <div className="mt-3 mx-8 justify-center">
-        {productCart.map((item, index) => (
-          <div key={index} className="flex items-center">
-            <img
-              src={item.product_image}
-              style={{ height: 200, width: 200 }}
-              alt=""
-            />
-            <div className="text-1xl ml-2 flex-grow">
-              <div className="flex justify-between items-center">
-                <p className="font-bold">{item.product_name}</p>
-                <div className="flex items-center">
-                  <button
-                    style={{ fontSize: "1.5em", padding: "0.2em 0.5em" }}
-                    onClick={() => handleDecrement(index)}
-                  >
-                    -
-                  </button>
-                  <span
-                    className="mx-2 font-bold"
-                    style={{ fontSize: "1.2em" }}
-                  >
-                    {item.quantity || 0}
-                  </span>
-                  <button
-                    style={{ fontSize: "1.5em", padding: "0.2em 0.5em" }}
-                    onClick={() => handleIncrement(index)}
-                  >
-                    +
-                  </button>
-                </div>
-              </div>
-              <div className="mt-3">
-                <p>{item.product_category}</p>
-              </div>
-              <div className="mt-3 ">
-                <p> {formatCurrency(item.product_price)},-</p>
-                <span
-                  className="mx-2 font-bold flex items-center mt-6 "
-                  style={{ fontSize: "1.2em" }}
-                >
-                  {item.quantity || 0} <p className="ml-3"> Barang </p>
-                </span>
-              </div>
+          {/* Display additional data from localStorage */}
+          <div className="text-md font-semibold my-3 space-y-3">
+            <p>Phone: {phone}</p>
+            <p>Province: {province}</p>
+            <p>District: {district}</p>
+            <p>Subdistrict: {subdistrict}</p>
+            <p>Zipcode: {zipcode}</p>
+          </div>
+
+          <div className="max-w-sm">
+            <div className="mb-2 block">
+              <Label htmlFor="selectCourier" value="Select your courier" />
             </div>
-          </div>
-        ))}
-      </div>
-
-      <Modal show={voucherModal} onClose={() => setVoucherModal(false)}>
-        <Modal.Header>Pilih Voucher</Modal.Header>
-        <Modal.Body>
-          {voucherData && voucherData.data && voucherData.data.length > 0 ? (
-            <>
-              <ul>
-                {voucherData.data.map((voucher) => (
-                  <li key={voucher.code}>
-                    <label>
-                      <input
-                        type="radio"
-                        name="voucher"
-                        onChange={() => handleVoucherSelect(voucher)}
-                      />
-                      {voucher.name} - Diskon {formatCurrency(voucher.discount)}
-                    </label>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) : (
-            <p>Data voucher kosong atau tidak valid.</p>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <FlowbiteButton onClick={() => setVoucherModal(false)}>
-            Tutup
-          </FlowbiteButton>
-        </Modal.Footer>
-      </Modal>
-
-      <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-8">
-        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          {/* ... */}
-        </table>
-
-        <div className="mb-4 text-3xl font-bold text-center">
-          Total: {formatCurrency(calculateTotalPrice())}
-        </div>
-
-        {useVoucher && selectedVoucher && (
-          <div className="text-center text-green-500">
-            Anda telah menggunakan voucher "{selectedVoucher.name}" dengan
-            diskon {formatCurrency(selectedVoucher.discount)}.
-          </div>
-        )}
-
-        <div className="flex flex-wrap justify-between p-3 px-8">
-          <div className="checkout-container">
-            <label>
-              <input
-                type="checkbox"
-                checked={useVoucher}
-                onChange={handleVoucherCheckbox}
-              />
-              Gunakan Voucher
-            </label>
-            <br /> {/* Add a line break */}
-            <FlowbiteButton
-              type="button"
-              className="mt-4 text-black custom-background font-semibold bg-blue-500 hover-bg-blue-800 focus-ring-4 focus-outline-none focus-ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark-bg-blue-600 dark-hover-bg-blue-700 dark-focus-ring-blue-800"
-              id="voucher-button"
-              onClick={() => setVoucherModal(true)}
-              disabled={!useVoucher}
+            <Select
+              id="selectCourier"
+              required
+              onChange={selectCourier}
+              value={selectedCourier}
             >
-              Pilih Voucher
-            </FlowbiteButton>
+              <option value="" disabled>
+                Select your courier
+              </option>
+              {courierData.map((data) => (
+                <option key={data._id} value={data.name}>
+                  {`${data.name} - ${data.description} (${data.etd}, Cost: ${data.cost})`}
+                </option>
+              ))}
+            </Select>
           </div>
-          <FlowbiteButton
-            type="button"
-            className="text-black custom-background font-semibold bg-yellow-300 hover-bg-blue-800 focus-ring-4 focus-outline-none focus-ring-blue-300 rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark-bg-blue-600 dark-hover-bg-blue-700 dark-focus-ring-blue-800"
-            id="checkout-button"
-          >
-            <a href="cart/address">Checkout</a>
-          </FlowbiteButton>
-        </div>
-      </div>
 
-      <Modal
-        show={openModal || deleteIndex !== null}
-        onClose={() => {
-          setOpenModal(false);
-          setDeleteIndex(null);
-        }}
-      >
-        <Modal.Header>Hapus Item</Modal.Header>
-        <Modal.Body>
-          {deleteIndex !== null ? (
-            <p>Apakah Anda yakin ingin menghapus item ini?</p>
-          ) : (
-            <p>Jumlah akan dikurangi menjadi 0. Apakah Anda yakin?</p>
-          )}
-        </Modal.Body>
-        <Modal.Footer>
-          <FlowbiteButton onClick={handleDeleteItem}>Ya</FlowbiteButton>
-        </Modal.Footer>
-      </Modal>
+          <Card className="mt-3">
+            <div>
+              Total:{" "}
+              <span className="font-semibold">
+                {formatCurrency(calculateTotalPrice())}
+              </span>
+            </div>
+            <div>
+              <Button className="" onClick={handleCheckout}>
+                <span>Checkout</span>
+              </Button>
+            </div>
+          </Card>
+        </Card>
+      </div>
     </>
   );
 }
 
-export default Cart;
+export default Checkout;
--- front-end/src/pages/Checkout.jsx
+++ front-end/src/pages/Checkout.jsx
@@ -4,11 +4,28 @@ import { FaCartShopping } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
 
-function Checkout() {
+const formatCurrency = (value) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(value);
+};
+
+function Checkout(props) {
   const [openModal, setOpenModal] = useState(false);
   const [courierData, setCourierData] = useState([]);
   const [selectedCourier, setSelectedCourier] = useState("");
   const token = JSON.parse(localStorage.getItem("token"));
+
+  // Additional data from localStorage
+  const phone = JSON.parse(localStorage.getItem("phone"));
+  const province = JSON.parse(localStorage.getItem("province"));
+  const district = JSON.parse(localStorage.getItem("district"));
+  const subdistrict = JSON.parse(localStorage.getItem("subdistrict"));
+  const zipcode = JSON.parse(localStorage.getItem("zipcode"));
+
+  // Additional product data from localStorage
+  const productData = props?.location?.state?.cart || [];
 
   useEffect(() => {
     axios(
@@ -26,108 +43,135 @@ function Checkout() {
         console.error("Error fetching courier data:", error);
         console.log("Error response data:", error.response.data);
       });
-  }, []);
+  }, [token]);
 
   const selectCourier = (event) => {
     setSelectedCourier(event.target.value);
   };
 
+  // Log productData and the first item
+  console.log("Product Data:", productData);
+  console.log("First Product:", productData[0]);
+
   return (
     <div>
-      <Breadcrumb
-        aria-label="Solid background breadcrumb example"
-        className="bg-gray-50 px-5 py-3 dark:bg-gray-800"
-      >
-        <Breadcrumb.Item href="#" icon={FaCartShopping}>
-          Home
-        </Breadcrumb.Item>
-        <Breadcrumb.Item href="#">Cart</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">Address</Breadcrumb.Item>
-        <Breadcrumb.Item href="#">Checkout</Breadcrumb.Item>
-      </Breadcrumb>
-
-      <div>
-        <Modal show={openModal} onClose={() => setOpenModal(false)}>
-          <Modal.Header>Terms of Service</Modal.Header>
-          <Modal.Body>
-            <div className="space-y-6">
-              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                With less than a month to go before the European Union enacts
-                new consumer privacy laws for its citizens, companies around the
-                world are updating their terms of service agreements to comply.
-              </p>
-              <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                The European Union’s General Data Protection Regulation
-                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
-                common set of data rights in the European Union. It requires
-                organizations to notify users as soon as possible of high-risk
-                data breaches that could personally affect them.
-              </p>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={() => setOpenModal(false)}>I accept</Button>
-            <Button color="gray" onClick={() => setOpenModal(false)}>
-              Decline
-            </Button>
-          </Modal.Footer>
-        </Modal>
-        <Card className="w-full">
-          <Card>
-            <h1 className="text-2xl font-semibold">
-              Tingkatkan keamanan akun anda!
-            </h1>
-            <p>
-              Tingkatkan keamanan akun anda dengan mengaktifkan autentikasi dua
-              faktor. Dengan menggunakan autentikasi dua faktor dapat memberikan
-              proteksi terhadap akun anda. Untuk lebih lengkapnya klik disini!.
+      <Modal show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header>Terms of Service</Modal.Header>
+        <Modal.Body>
+          <div className="space-y-6">
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              With less than a month to go before the European Union enacts new
+              consumer privacy laws for its citizens, companies around the world
+              are updating their terms of service agreements to comply.
             </p>
-            <diV>
-              <Button className="" onClick={() => setOpenModal(true)}>
-                Mengerti
-              </Button>{" "}
-            </diV>
-          </Card>
-
-          <h1 className="text-2xl font-semibold">Address</h1>
-
-          <div className="text-md font-semibold my-3 space-y-3">
-            {/* ... other content ... */}
+            <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+              The European Union’s General Data Protection Regulation (G.D.P.R.)
+              goes into effect on May 25 and is meant to ensure a common set of
+              data rights in the European Union. It requires organizations to
+              notify users as soon as possible of high-risk data breaches that
+              could personally affect them.
+            </p>
           </div>
-          <div className="max-w-sm">
-            <div className="mb-2 block">
-              <Label htmlFor="selectCourier" value="Select your courier" />
-            </div>
-            <Select
-              id="selectCourier"
-              required
-              onChange={selectCourier}
-              value={selectedCourier}
-            >
-              <option value="" disabled>
-                Select your courier
-              </option>
-              {courierData.map((data) => (
-                <option key={data._id} value={data.name}>
-                  {`${data.name} - ${data.description} (${data.etd}, Cost: ${data.cost})`}
-                </option>
-              ))}
-            </Select>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={() => setOpenModal(false)}>I accept</Button>
+          <Button color="gray" onClick={() => setOpenModal(false)}>
+            Decline
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Card className="w-full">
+        <Card>
+          <h1 className="text-2xl font-semibold">
+            Tingkatkan keamanan akun anda!
+          </h1>
+          <p>
+            Tingkatkan keamanan akun anda dengan mengaktifkan autentikasi dua
+            faktor. Dengan menggunakan autentikasi dua faktor dapat memberikan
+            proteksi terhadap akun anda. Untuk lebih lengkapnya klik disini!.
+          </p>
+          <div>
+            <Button className="" onClick={() => setOpenModal(true)}>
+              Mengerti
+            </Button>{" "}
           </div>
-          <Card className="mt-3">
-            <div>
-              Total: <span className="font-semibold">Rp. 100.000</span>
-            </div>
-            <div>
-              <Button className="">
-                <NavLink to={"payment"}>
-                  <span>payment</span>
-                </NavLink>
-              </Button>
-            </div>
-          </Card>
         </Card>
-      </div>
+
+        <h1 className="text-2xl font-semibold">Address</h1>
+
+        {/* Display additional data from localStorage */}
+        <div className="text-md font-semibold my-3 space-y-3">
+          <p>Phone: {phone}</p>
+          <p>Province: {province}</p>
+          <p>District: {district}</p>
+          <p>Subdistrict: {subdistrict}</p>
+          <p>Zipcode: {zipcode}</p>
+        </div>
+
+        {/* Display product data */}
+        <div className="mt-3 mx-8 justify-center">
+          {productData.map((item, index) => (
+            <div key={index} className="flex items-center">
+              <img
+                src={item.product_image}
+                style={{ height: 200, width: 200 }}
+                alt=""
+              />
+              <div className="text-1xl ml-2 flex-grow">
+                <div className="flex justify-between items-center">
+                  <p className="font-bold">{item.product_name}</p>
+                </div>
+                <div className="mt-3">
+                  <p>{item.product_category}</p>
+                </div>
+                <div className="mt-3">
+                  <p>{formatCurrency(item.product_price)},-</p>
+                  <span
+                    className="mx-2 font-bold flex items-center mt-6"
+                    style={{ fontSize: "1.2em" }}
+                  >
+                    {item.quantity || 0} <p className="ml-3"> Barang </p>
+                  </span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className="max-w-sm">
+          <div className="mb-2 block">
+            <Label htmlFor="selectCourier" value="Select your courier" />
+          </div>
+          <Select
+            id="selectCourier"
+            required
+            onChange={selectCourier}
+            value={selectedCourier}
+          >
+            <option value="" disabled>
+              Select your courier
+            </option>
+            {courierData.map((data) => (
+              <option key={data._id} value={data.name}>
+                {`${data.name} - ${data.description} (${data.etd}, Cost: ${data.cost})`}
+              </option>
+            ))}
+          </Select>
+        </div>
+
+        <Card className="mt-3">
+          <div>
+            Total: <span className="font-semibold">Rp. 100.000</span>
+          </div>
+          <div>
+            <Button className="">
+              <NavLink to={"payment"}>
+                <span>payment</span>
+              </NavLink>
+            </Button>
+          </div>
+        </Card>
+      </Card>
     </div>
   );
 }
