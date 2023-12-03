2f6bed0bc591e863504c586eb534536e32c9d199
naufyl28 <11211060@student.itk.ac.id>
2023-12-04 01:53:57 +0800

    styling navbar footer

--- front-end/src/components/Footer.jsx
+++ front-end/src/components/Footer.jsx
@@ -10,48 +10,77 @@ import {
 
 function Component() {
   return (
-    <Footer container>
-      <div className="w-full mx-4">
-        <div className="grid lg:grid-cols-3  w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
+    <Footer
+      container
+      fluid
+      style={{ backgroundColor: "#023047", color: "#ffffff" }}
+    >
+      <div className="w-full h-full">
+        <div className="grid lg:grid-cols-3 w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1 gap-4 items-stretch">
           <div className="mt-4">
             <Footer.Brand
               href="#"
               src={Logo}
               alt="Furnivul Logo"
-              name="Furnivul"
+              name={<span className="text-yellow-300">Furnivul</span>}
+              className="text-white"
             />
-            <div className="my-8 text-blue-900">
-              <span>Furnivul</span> berdiri sejak nenek moyang <br></br> telah
-              lahir menggunakan goresan tangan <br></br> sesepuh, dengan
-              kearifan lokal yang mendunia.{" "}
+            <div className="my-8">
+              <span className="text-yellow-300">Furnivul</span> berdiri sejak
+              nenek moyang <br></br> telah lahir menggunakan goresan tangan{" "}
+              <br></br> sesepuh, dengan kearifan lokal yang mendunia.
             </div>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-6">
             <div>
-              <Footer.Title title="Home" />
+              <Footer.Title title="Home" className="text-white" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Kitchen</Footer.Link>
-                <Footer.Link href="#">Bed Room</Footer.Link>
-                <Footer.Link href="#">Living Room</Footer.Link>
-                <Footer.Link href="#">Dining Room</Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Kitchen
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Bed Room
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Living Room
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Dining Room
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="Category" />
+              <Footer.Title title="Category" className="text-white" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Work Space</Footer.Link>
-                <Footer.Link href="#">OutDoor</Footer.Link>
-                <Footer.Link href="#">Child Room</Footer.Link>
-                <Footer.Link href="#">Accesories</Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Work Space
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  OutDoor
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Child Room
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Accesories
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
-              <Footer.Title title="Utilities" />
+              <Footer.Title title="Utilities" className="text-white" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Article</Footer.Link>
-                <Footer.Link href="#">Category</Footer.Link>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
-                <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Article
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Category
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Privacy Policy
+                </Footer.Link>
+                <Footer.Link href="#" className="text-white">
+                  Terms &amp; Conditions
+                </Footer.Link>
               </Footer.LinkGroup>
             </div>
           </div>
@@ -62,13 +91,34 @@ function Component() {
             href="#"
             by="Furnivul™ All rights reserved"
             year={2023}
+            className="text-white"
           />
           <div className="mt-4  flex gap-2 sm:mt-0 sm:justify-center">
-            <Footer.Icon href="#" icon={BsFacebook} />
-            <Footer.Icon href="#" icon={BsInstagram} />
-            <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
-            <Footer.Icon href="#" icon={BsDribbble} />
+            <Footer.Icon
+              href="#"
+              icon={BsFacebook}
+              style={{ color: "#FFB703" }}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsInstagram}
+              style={{ color: "#FFB703" }}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsTwitter}
+              style={{ color: "#FFB703" }}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsGithub}
+              style={{ color: "#FFB703" }}
+            />
+            <Footer.Icon
+              href="#"
+              icon={BsDribbble}
+              style={{ color: "#FFB703" }}
+            />
           </div>
         </div>
       </div>
--- front-end/src/components/Navbar.jsx
+++ front-end/src/components/Navbar.jsx
@@ -1,8 +1,6 @@
-import { Avatar, Button, Card, Dropdown, Navbar } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar } from "flowbite-react";
 import { NavLink, useNavigate } from "react-router-dom";
 import Logo from "../assets/images/logo.png";
-
-// ... (your existing imports)
 
 function NavbarComponent() {
   const isLoggedIn = JSON.parse(localStorage.getItem("idUser"));
@@ -20,88 +18,80 @@ function NavbarComponent() {
   let component = "";
   if (isLoggedIn) {
     component = (
-      <>
-        {" "}
-        <div className="flex">
-          <div className="mt-2 mx-3 ">{nameUser}</div>
-          <Dropdown
-            arrowIcon={false}
-            inline
-            label={<Avatar alt="User settings" img={profileUser} rounded />}
-          >
-            <Dropdown.Header>
-              <span className="block text-sm">{nameUser}</span>
-              <span className="block truncate text-sm font-medium">
-                {emailUser}
-              </span>
-            </Dropdown.Header>
-            <Dropdown.Item>Dashboard</Dropdown.Item>
-            <Dropdown.Item>Settings</Dropdown.Item>
-            <NavLink to={"/cart"}>
-              <span>
-                {" "}
-                <Dropdown.Item>Cart</Dropdown.Item>
-              </span>{" "}
-            </NavLink>
-
-            <Dropdown.Divider />
-            <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
-          </Dropdown>
-        </div>
-      </>
+      <div className="flex items-center">
+        <div className="mx-3 text-white">{nameUser}</div>
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={<Avatar alt="User settings" img={profileUser} rounded />}
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">{nameUser}</span>
+            <span className="block truncate text-sm font-medium">
+              {emailUser}
+            </span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <NavLink to={"/cart"}>
+            <span>
+              <Dropdown.Item>Cart</Dropdown.Item>
+            </span>
+          </NavLink>
+          <Dropdown.Divider />
+          <Dropdown.Item onClick={handleLogout}>Sign out</Dropdown.Item>
+        </Dropdown>
+      </div>
     );
   } else {
     component = (
-      <>
-        <div className="flex gap-1 mx-auto">
-          <Button className=" ">
-            <NavLink to={"/login"} className="">
-              <span>Login</span>
-            </NavLink>
-          </Button>
-          <Button>
-            <NavLink to={"/register"} className=" text-white ">
-              <span> Register</span>
-            </NavLink>
-          </Button>
-        </div>
-      </>
+      <div className="flex gap-1">
+        <Button className="">
+          <NavLink to={"/login"} className="text-white">
+            <span>Login</span>
+          </NavLink>
+        </Button>
+        <Button>
+          <NavLink to={"/register"} className="text-white">
+            <span>Register</span>
+          </NavLink>
+        </Button>
+      </div>
     );
   }
 
   return (
-    <div className=" justify-center  my-2  mx-4">
-      <Navbar fluid>
+    <div className="my-0 mx-0">
+      <Navbar fluid style={{ backgroundColor: "#023047" }}>
         <Navbar.Brand onClick={() => navigate("/")}>
           <img
             src={Logo}
-            className=" mr-3  sm:h-9 flex-start"
+            className="mr-3 sm:h-9 flex-start"
             style={{ height: 35, width: 38 }}
             alt="Flowbite React Logo"
           />
-          <span className="text-2xl font-bold dark:text-white">Furnivul</span>
+          <span className="text-2xl font-bold text-white">Furnivul</span>
         </Navbar.Brand>
 
         <Navbar.Collapse className="">
           <NavLink to={"/"} className="mt-1">
             <Navbar.Link>
-              <span>Home</span>
+              <span className="text-white">Home</span>
             </Navbar.Link>
           </NavLink>
           <NavLink to={"/category-product"} className="mt-1">
             <Navbar.Link>
-              <span>Category</span>
+              <span className="text-white">Category</span>
             </Navbar.Link>
           </NavLink>
-
           <NavLink to={"/article"} className="mt-1">
             <Navbar.Link>
-              <span>Article</span>
+              <span className="text-white">Article</span>
             </Navbar.Link>
           </NavLink>
         </Navbar.Collapse>
 
-        <div className="">{component}</div>
+        <div className="text-white">{component}</div>
 
         <Navbar.Toggle />
       </Navbar>
